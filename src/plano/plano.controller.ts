b10558706ee19e8ca9cccb4ebcf99c4e2ed080d3
seguel <ariel.alejandrocs@gmail.com>
2026-01-16 17:38:49 -0300

    ajustes dos planos/pagtos

--- src/plano/plano.controller.ts
+++ src/plano/plano.controller.ts
@@ -8,16 +8,21 @@ import {
   Req,
   Post,
   Query,
+  Headers,
 } from '@nestjs/common';
 import { PlanoService } from './plano.service';
 import { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { plano } from '@prisma/client';
+import { I18nService } from 'nestjs-i18n';
 
 @Controller('planos')
 export class PlanoController {
-  constructor(private readonly planoService: PlanoService) {}
+  constructor(
+    private readonly planoService: PlanoService,
+    private readonly i18n: I18nService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Get(':id')
@@ -27,10 +32,11 @@ export class PlanoController {
 
   @Get()
   getPlanos(
+    @Headers('accept-language') language: string,
     @Req() req: Request & { user: JwtPayload },
     @Query('perfilId') perfilId?: string,
   ) {
-    const lang = req.user?.lang ?? 'pt';
+    const lang = req.user?.lang ?? language;
     return this.planoService.getPlanos(
       lang,
       perfilId ? Number(perfilId) : undefined,
@@ -40,14 +46,22 @@ export class PlanoController {
   @UseGuards(JwtAuthGuard)
   @Post()
   postPlano(
-    @Body() body: { perfilId: number; planoPeriodoId: number },
+    @Body()
+    body: {
+      perfilId: number;
+      planoPeriodoId: number;
+      token_pagto: string;
+    },
     @Req() req: Request & { user: JwtPayload },
   ) {
     const userId = req.user?.sub;
+    const lang = req.user?.lang ?? 'pt';
     return this.planoService.postPlano(
       userId,
       body.perfilId,
       body.planoPeriodoId,
+      lang,
+      body.token_pagto,
     );
   }
 }
