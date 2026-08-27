202cb36bedc0962984109abff8e40d1768320c76
seguel <ariel.alejandrocs@gmail.com>
2026-08-27 13:13:00 -0300

    ajuste migration

--- prisma/migrations/20260826221818_ajuste_coluna_modalidade_obg/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `codigo` on table `modalidade_trabalho` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "modalidade_trabalho" ALTER COLUMN "codigo" SET NOT NULL;
